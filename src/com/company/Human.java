2d1210c3e611471e365b22721b4a5f9da1078931
cruzoanton <74861347+cruzoanton@users.noreply.github.com>
2020-12-28 23:51:18 +0200

    gotovo

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -20,63 +20,80 @@ public class Human {
     public Boolean talk(boolean gend) {  //сверяем только пол, остальные параметры не имеют значение
         System.out.print("Будут ли говорить?  ");
         if (!gender && !gend) {
+            System.out.println("Да!");
             return true;
         } else if (!gender && gend) {
+            System.out.println("Да!");
             return true;
         } else if (gender && !gend) {
+            System.out.println("Да!");
             return true;
         } else if (gender && gend) {
             Random r = new Random();
             if (r.nextInt(100) < 50) {
+                System.out.println("Да!");
                 return true;
-            } else
+            } else {
+                System.out.println("Нет!");
                 return false;
+            }
         }
+        System.out.println("Нет!");
         return false;
+
     }
 
-    public Boolean company(boolean gend) {
+    public Boolean company(boolean gend) { //сверка терпения по половым признакам
         System.out.print("Будут ли терпеть общество друг друга?  ");
         if (!gender && !gend) {
             Random r = new Random();
             if (r.nextInt(100) < 5) {
+                System.out.println("Да!");
                 return true;
             }
         } else if (!gender && gend) {
             Random r = new Random();
             if (r.nextInt(100) < 70) {
+                System.out.println("Да!");
                 return true;
             }
         } else if (gender && !gend) {
             Random r = new Random();
             if (r.nextInt(100) < 70) {
+                System.out.println("Да!");
                 return true;
             }
         } else if (gender && gend) {
             Random r = new Random();
             if (r.nextInt(100) < 5.6) {
+                System.out.println("Да!");
                 return true;
             }
         }
         {
+            System.out.println("Нет!");
             return false;
         }
     }
 
-    public Boolean together(float h) {
+    public Boolean together(float h) { //тут вместо полового признака сверяем рост
         System.out.print("Будут тусить вместе?  ");
         if (Math.abs(height - h) < height / 10) {    //разница в росте до 10%
-            Random rTog1 = new Random();
-            if (rTog1.nextInt(100) < 95) {
+            Random r = new Random();
+            if (r.nextInt(100) < 95) {
+                System.out.println("Да, вероятность была 95%");
                 return true;
             }
-        } else {//разница более 10%
-            Random rTog2 = new Random();
-            if (rTog2.nextInt(100) < 85) {
+        } else {                                        //разница более 10%
+            Random r = new Random();
+            if (r.nextInt(100) < 85) {
+                System.out.println("Да, вероятность была 85%");
                 return true;
             }
+            System.out.println("Нет, вероятность была 15%");
             return false;
         }
+        System.out.println("Нет, вероятность была 5% (");
         return false;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,59 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
-
+//в своем ДЗ я не стал использовать сканер, т.к. это очень замедляет подбор различных вариантов
     public static void main(String[] args) {
-        Human human1 = new Human(true, "Иван", "Костенко", 180, 95);
-        Human human2 = new Human(false, "Лариса", "Петренко", 160, 57);
+        float rost, ves; //переменные для ребенка
+        Human human1 = new Human(false, "Иван", "Костенко", 180, 95); //тут вводим наши варианты
+        Human human2 = new Human(true, "Лариса", "Петренко", 180, 57);
 
-
-        System.out.println(human1.toString());
+        System.out.println(human1.toString()); //вывести наших людей на экран
         System.out.println(human2.toString());
-        System.out.println(human1.talk(human2.gender)); //берем только пол
-        System.out.println(human1.company(human2.gender));
-        System.out.println(human1.together(human2.height)); //здесь сравниваем рост
+//        System.out.println(human1.talk(human2.gender)); // выводить отдельно каждый метод
+//        System.out.println(human1.company(human2.gender));
+//        System.out.println(human1.together(human2.height));
+
+        if (human1.talk(human2.gender) &&  //сравниваем условия методов
+                human1.company(human2.gender) &&
+                human1.together(human2.height)) {
+            System.out.println("Поздравляем!");
+            Random r = new Random();
+
+            if (human1.gender != human2.gender) {
+                System.out.println("У Вас родился малыш!");
+                if (r.nextInt(100) < 50) {
+                    System.out.print("Мальчик, будет ");
+                    if (human1.gender) {             //присваиваем вес и рост
+                        rost = (float) (human1.height + human2.height * 0.1); //если первый родитель отец
+                        ves = (float) (human1.weight + human2.weight * 0.1);
+                    } else {
+                        rost = (float) (human2.height + human1.height * 0.1); //если второй родитель отец
+                        ves = (float) (human2.weight + human1.weight * 0.1);
+                    }
 
 
+                } else {
+                    System.out.print("Девочка, будет "); //то же самое но с девочкой, если первый родитель мать
+                    if (!human1.gender) {
+                        rost = (float) (human1.height + human2.height * 0.1);
+                        ves = (float) (human1.weight + human2.weight * 0.1);
+                    } else {
+                        rost = (float) (human2.height + human1.height * 0.1);
+                        ves = (float) (human2.weight + human1.weight * 0.1);
+                    }
+                }
+
+                if (human1.gender) {            //присваиваем Фамилию и закидываем рост и вес ребенка
+                    System.out.print(human1.surName + " " + "ростом " + rost + " и весом " + ves);
+                } else
+                    System.out.print(human2.surName + " " + "ростом " + rost + " и весом " + ves);
+            }
+
+
+        } else System.out.println("Ничего не вышло");
 
     }
 }
+
