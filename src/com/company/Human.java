77d4731747831ae44f9a3c122467d1785c267c58
cruzoanton <74861347+cruzoanton@users.noreply.github.com>
2021-01-05 13:12:25 +0200

    v 3.0 with scanner

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Human {
     Boolean gender;
@@ -18,7 +19,7 @@ public class Human {
         this.weight = weight;
     }
 
-    public Boolean talk(Object a, Object b) {  //сверяем только пол, остальные параметры не имеют значения
+    protected Boolean talk(Object a, Object b) {  //сверяем только пол, остальные параметры не имеют значения
         Human first = (Human) a;
         Human second = (Human) b;
         System.out.print("Будут ли говорить?  ");
@@ -45,7 +46,7 @@ public class Human {
         return false;
     }
 
-    public Boolean company(Object a, Object b) { //сверка терпения по половым признакам
+    protected Boolean company(Object a, Object b) { //сверка терпения по половым признакам
         Human first = (Human) a;
         Human second = (Human) b;
         System.out.print("Будут ли терпеть общество друг друга?  ");
@@ -80,7 +81,7 @@ public class Human {
         }
     }
 
-    public Boolean together(Object a, Object b) { //тут вместо полового признака сверяем рост
+    protected Boolean together(Object a, Object b) { //тут вместо полового признака сверяем рост
         Human first = (Human) a;
         Human second = (Human) b;
         System.out.print("Будут тусить вместе?  ");
@@ -103,7 +104,7 @@ public class Human {
         return false;
     }
 
-    public void makeBaby(Object a, Object b) {
+    protected void makeBaby(Object a, Object b) {
         Human first = (Human) a;
         Human second = (Human) b;
         if (talk(first, second) &&  //сравниваем условия методов
@@ -137,9 +138,9 @@ public class Human {
                 }
 
                 if (first.gender) {            //присваиваем Фамилию и закидываем рост и вес ребенка
-                    System.out.print(first.surName + " " + "ростом " + rost + " и весом " + ves);
+                    System.out.println(first.surName + " " + "ростом " + rost + " и весом " + ves);
                 } else
-                    System.out.print(second.surName + " " + "ростом " + rost + " и весом " + ves);
+                    System.out.println(second.surName + " " + "ростом " + rost + " и весом " + ves);
             }
         } else System.out.println("Ничего не вышло");
     }
@@ -148,5 +149,25 @@ public class Human {
     public String toString() {
         String pol = gender ? "Мужчина " : "Женщина ";
         return pol + name + " " + surName + " Ростом= " + height + "см и Весом= " + weight + "кг";
+    }
+    protected static Human scanHuman() {//метод сканирования объекта
+        Scanner scan = new Scanner(System.in);
+        boolean a;
+        String b, c;
+        float d, e;
+        System.out.println("Пол (m для мужчины)");
+        a = scan.hasNext("m");
+        scan.nextLine();
+        System.out.println("Имя");
+        b = scan.next();
+        System.out.println("Фамилия");
+        c = scan.next();
+        System.out.println("рост");
+        d = scan.nextFloat();
+        System.out.println("вес");
+        e = scan.nextFloat();
+        scan.nextLine();
+        Human hum = new Human(a, b, c, d, e);
+        return hum;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,27 @@
 package com.company;
 
 public class Main {
-    //в своем ДЗ я не стал использовать сканер, т.к. это очень замедляет подбор различных вариантов
     public static void main(String[] args) {
         Human tinder = new Human(false, "Регистратор", "Тиндера", 160, 90); //Используем для обращения за методами в другом классе
-        Human human1 = new Human(true, "Иван", "Костенко", 180, 95); //тут вводим наши варианты
-        Human human2 = new Human(false, "Лариса", "Петренко", 170, 57);
+        Human scanned1 = Human.scanHuman(); //сканируемый объект
+        Human human2 = new Human(true, "Иван", "Костенко", 180, 95); //тут наши готовые варианты
+        Human human3 = new Human(false, "Лариса", "Петренко", 170, 57);
 
-        System.out.println(human1.toString()); //вывести наших людей на экран
-        System.out.println(human2.toString());
-//        tinder.talk(human1,human2);  //каждый метод по отдельности
-//        tinder.company(human1,human2);
-//        tinder.together(human1,human2);
-        tinder.makeBaby(human1, human2); //все методы вместе
+        System.out.println("1. " + scanned1.toString());
+        System.out.println("2. " + human2.toString()); //вывести наших людей на экран
+        System.out.println("3. " + human3.toString());
+
+        System.out.println("Сверяем 1 и 2:");
+        tinder.makeBaby(scanned1, human2);
+        System.out.println("Сверяем 1 и 3:");
+        tinder.makeBaby(scanned1, human3);
+        System.out.println("Сверяем 2 и 3:");
+        tinder.makeBaby(human2, human3);
+
+//        tinder.talk(human2,human3);        //методы отдельно по желанию
+//        tinder.company(human2,human3);
+//        tinder.together(human2,human3);
     }
 }
+
 
